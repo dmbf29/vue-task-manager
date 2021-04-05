965cf2f37ad4764f82e0628d6a83d1e859d3fbae
Douglas Berkley <douglasmberkley@gmail.com>
2021-04-05 20:23:48 +0900

    added task form

--- src/components/TasksList.vue
+++ src/components/TasksList.vue
@@ -2,7 +2,21 @@
   <div id="tasks">
     <button
       class="btn round-icon"
-      @click="addTask('My new task', 'My new description')">+</button>
+      @click="addTask(newTitle, newDescription)">+</button>
+
+    <div class="task-card new-task">
+      <div>
+        <input
+          type="text"
+          placeholder="What would you like to do?"
+          v-model="newTitle"
+        />
+        <textarea
+          placeholder="Add some details about your task..."
+          v-model="newDescription"
+        ></textarea>
+      </div>
+    </div>
 
     <div v-if="tasks.length > 0">
       <TaskCard
@@ -28,6 +42,8 @@ export default {
   data() {
     return {
       tasks: [],
+      newTitle: '',
+      newDescription: ''
     };
   },
 
@@ -38,4 +54,18 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+@keyframes expand-vertical { from { min-height: 0; height: 0; } to { min-height: 6rem; } }
+.task-card.new-task {
+  animation: expand-vertical 0.2s;
+  overflow: hidden;
+  background-color: white;
+  border-left: solid 5px #35495e;
+  &, &:hover { transform: scale(1.1); box-shadow: 2px 3px 10px rgba(black, 0.2); }
+  & + .tasks-list { pointer-events: none; }
+  input { font-size: 1.17rem; font-weight: bold; width: 100%; }
+  textarea { width: 100%; font-size: 0.9rem; resize: none; }
+}
+</style>
 
