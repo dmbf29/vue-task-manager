40b79a8c841fc3824499fb137b6a702b493cd268
Douglas Berkley <douglasmberkley@gmail.com>
2021-04-05 19:23:22 +0900

    initial commit frame

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" height="100" />
+    <h1>Task Manager</h1>
+    <TasksList></TasksList>
+  </div>
+</template>
+
+<script>
+import TasksList from './components/TasksList'
+
+export default {
+  name: "App",
+
+  components: { TasksList },
+};
+</script>
+
+<style lang="scss">
+@import "./assets/style.scss";
+</style>
---
+++ src/components/TasksList.vue
@@ -0,0 +1,8 @@
+<template>
+  <div id="tasks">
+    <div class="task-card">
+      <h3>Title of Card</h3>
+      <p>Description</p>
+    </div>
+  </div>
+</template>
